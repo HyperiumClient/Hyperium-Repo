e07540c8b591b429056aae169231e13303b3392b
Systemless <45434286+Systemless03@users.noreply.github.com>
2019-11-09 17:41:06 +0100

    i hate every single macos user

---
+++ files/yougivemepain.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" != "darwin" ]]; then
+    echo "You\'re not on MacOS go away >:("
+    exit 0
+fi
+
+echo "========================="
+echo "The anti-MacOSUser anti-java 9+ script!"
+echo "This script will remove java."
+echo "Do you want to continue? Y/n"
+echo "========================="
+
+read asbyth
+if [[ $asbyth == "n" || $asbyth == "N" ]]; then
+    clear
+    echo "Cancelled"
+    exit 0
+fi
+
+
+clear
+echo "Gamer moment"
+
+# Got those commands from https://www.java.com/en/download/help/mac_uninstall_java.xml
+    #sudo rm -rf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin
+    #sudo rm -rf /Library/PreferencePanes/JavaControlPanel.prefPane
+    #sudo rm -rf ~/Library/Application\ Support/Oracle/Java
+    clear
+
+echo "Opening java\'s website..."
+open https://www.java.com/en/download/mac_download.jsp
+
+echo "goodbye."
